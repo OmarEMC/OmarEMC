99f5a88f46ba7ebdd29d77e55c659a3d470468cf
OmarEMC <omar.io.3203@gmail.com>
2021-06-26 23:54:15 -0500

    Applied next lint integration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['plugin:react/recommended', 'standard'],
+  extends: ['plugin:react/recommended', 'plugin:@next/next/recommended', 'standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -20,6 +20,7 @@ module.exports = {
     }
   },
   rules: {
+    '@next/next/no-img-element': 0,
     'react/react-in-jsx-scope': 0,
     indent: ['error', 2]
   }
