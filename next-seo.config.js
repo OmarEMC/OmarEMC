f790196d7a9d53d782ae57cd5a207af2c6f628fc
Omar Moreno <51929737+OmarEMC@users.noreply.github.com>
2023-07-14 10:51:35 -0500

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,12 @@
 export default {
   titleTemplate: 'Omar Moreno - %s',
   defaultTitle: 'Omar Moreno',
-  description: '¡Hola! Soy Omar Moreno, tengo 17 años y soy desarrollador full stack JavaScript & TypeScript.',
+  description: '¡Hola! Soy Omar Moreno, tengo 18 años y soy desarrollador full stack JavaScript & TypeScript.',
   openGraph: {
     type: 'website',
     site_name: 'Omar Moreno',
     title: 'Omar Moreno',
-    description: '¡Hola! Soy Omar Moreno, tengo 17 años y soy desarrollador full stack JavaScript & TypeScript.',
+    description: '¡Hola! Soy Omar Moreno, tengo 18 años y soy desarrollador full stack JavaScript & TypeScript.',
     locale: 'es_CO',
     url: 'https://omaremc.vercel.app/'
   },
